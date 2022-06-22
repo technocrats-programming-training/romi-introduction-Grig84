d065e22d905b784943620b5f3adaf7d71b5c5204
Will D <44533697+Grig84@users.noreply.github.com>
2022-06-22 13:22:59 +0000

    Added Auto

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,7 @@ public class Robot extends ProceduralRobot {
   @Override
   public void autonomousProcedure() throws InterruptedException {
     // Write your code here:
-
+    DriveTime(1,1);
+    TurnTime(3,.5);
   }
 }
